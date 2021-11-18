35a115ed767f2280a2beb17b923e6736feb4fb49
zer0 <zer0@example.com>
2021-11-18 09:36:07 +0900

    changed logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
+import {useState, useEffect} from 'react'
+
+import axios from 'axios';
+
 
 function App() {
+  const [users, setUsers] = useState([])
+  console.log("sending")
+
+  axios.get('http://localhost:3001/')
+  .then((res) => console.log(res))
+  .catch(err => console.log(err))
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Form/>
+      {users}
     </div>
   );
+}
+
+function Form () {
+  return (
+    <div className="disc">
+      <h1>hello world</h1>
+      <form action="/api" method="POST">
+        <input type="text" name="inputText" id="inputText"/>
+          <input type="submit" value="Generate!"/>
+        </form>
+        </div>
+  )
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -4,56 +4,56 @@ const app = express()
 
 // app.use(cors)
 
-var row = 20;
-var column = 80;
-
-test = [
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1]
-]
 
 
-function generateContainer () {
-    var columnArr = []
-    var rowArr = []
-    for (var j = 0; j < column; j++) {
-        columnArr.push(0)
-    }
-    for (var j = 0; j < row; j++) {
-        rowArr.push(columnArr)
-    }
-    var container = rowArr
-    return container
-}
-
-
-function generatePixel (container) {
-    for(var i = 0; i < row; i++){
-        for(var j = 0; j < column; j++){
-            if(test[i][j] == 1){
-                container[i][j] = 1;
-            }
-        }
-    }
-}
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/api', (req, res) => {
-    console.log(req.body.inputText)
     generatePixel()
     res.redirect('/')
 })
 
-app.get('/', (req, res) => {
-    var pixel = generateContainer()
-    res.render("index", {pixel: pixel})
+app.get('/', async(req, res) => {
+    var row = 7;
+    var column = 80;
+
+    test = [
+        [1, 1, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 1],
+        [0, 1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 1]
+    ]
+    var x = test.length
+    var y = test[1].length
+
+        var columnArr = []
+        var rowArr = []
+        for (var j = 0; j < column; j++) {
+            columnArr.push(0)
+        }
+        for (var j = 0; j < row; j++) {
+            rowArr.push(columnArr)
+        }
+        var container = rowArr
+
+
+
+
+        for (var i = 0; i < x; i++) {
+            for (var j = 0; j < y; j++) {
+                container[i][j] = test[i][j]
+            }
+        }
+
+
+
+        console.log(container)
+    res.render("index2", {pixel: container})
 })
 
 
